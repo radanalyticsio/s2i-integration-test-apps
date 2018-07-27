d03b503756b2ca3920e2ca1cc13f8d4a9ce008e7
Trevor McKay <tmckay@redhat.com>
2018-07-27 09:06:45 -0400

    Add a completion string to java/scala test apps (for test scripts)

--- src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -37,6 +37,7 @@ object SparkPi {
       if (x*x + y*y < 1) 1 else 0
     }.reduce(_ + _)
     println("Pi is roughly " + 4.0 * count / (n - 1))
+    println("test app completed")
     spark.stop()
   }
 }
