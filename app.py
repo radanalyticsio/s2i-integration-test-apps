1aec75440fdfcc4b3992d33472b9badf1d476b3b
Michael McCune <elmiko@redhat.com>
2019-10-28 11:55:08 -0400

    refactor python app to be a copy of sparkpi
    previously the app was just counting a collection of tuples, now it
    estimates pi as the other (java,scala) apps do.

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
-# spark-basic.py
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
 import sys
-import time
-import numpy
+from random import random
+from operator import add
 
-from pyspark import SparkConf
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
-conf = SparkConf()
-conf.setAppName('spark-basic')
-sc = SparkContext(conf=conf)
 
-def mod(x):
-    return (x, numpy.mod(x, 2))
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
 
-print('\n'.join(sys.path))
-rdd = sc.parallelize(range(1000000)).map(mod).take(30)
-print(rdd)
-print("test app completed")
-time.sleep(60)
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * partitions
+
+    def f(_):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+    print("test app completed")
+
+    spark.stop()
